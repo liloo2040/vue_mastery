1d8bf4c84f6e6067e1d5955a67dc0a7e688fa333
liloo2040 <e.dufour.89@gmail.com>
2019-02-23 18:05:42 +0100

    working with binding

--- intro/main.js
+++ intro/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        // description: "A pair of warm, fuzzy socks"
+        image: './assets/vmSocks-green.jpg',
+        link:'https://vue.js.org'
     }
 })
