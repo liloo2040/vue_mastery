e36c73b42e170c689e96a15fcab7f2bf61506079
liloo2040 <e.dufour.89@gmail.com>
2019-02-23 22:52:35 +0100

    working with v-for

--- intro/main.js
+++ intro/main.js
@@ -6,6 +6,18 @@ var app = new Vue({
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         link:'https://vue.js.org', 
         inStock: true, 
-        onSale: true
+        onSale: true, 
+        details: ["80% cotton", "20% neutral", "Gender neutral"], 
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ], 
+        sizes: ["M","XS", "L", "XL", "S"]
     }
 })
