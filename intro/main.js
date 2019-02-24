616b2946ad1f55f3aed75a8c1c12f19747ba0d6c
liloo2040 <e.dufour.89@gmail.com>
2019-02-24 23:16:25 +0100

    working with new methods

--- intro/main.js
+++ intro/main.js
@@ -1,38 +1,59 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
+        product: 'Socks',
+        brand: 'Vue Mastery',
         // description: "A pair of warm, fuzzy socks"
-        image: './assets/vmSocks-green.jpg',
+        selectedVariant: 0,
         link:'https://vue.js.org', 
-        inStock: false, 
         /* onSale: true,  */
         details: ["80% cotton", "20% neutral", "Gender neutral"], 
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green', 
-                variantImage: './assets/vmSocks-green.jpg'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue.jpg'
-            }
+    variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green', 
+            variantImage: './assets/vmSocks-green.jpg',
+            variantQuantity: 10
+        },
+        {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: './assets/vmSocks-blue.jpg', 
+            variantQuantity: 0
+        }
         ], 
-        cart: 0
-        }, 
-        methods: {
+    cart: 0, 
+    onSale: true
+    }, 
+    methods: {
             addToCart() {
                 this.cart += 1
             },
             deleteFromCart(){
                 this.cart -= 1
             },
-            updateProduct(variantImage) {
-                this.image = variantImage
+            updateProduct(index) {
+                this.selectedVariant = index
+                console.log(index)
             }
+    },
+    computed:{
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+                return this.brand + ' ' + this.product + ' are not on sale!'
+            }
+    }
         //, 
         // sizes: ["M","XS", "L", "XL", "S"]
-    }
 })
